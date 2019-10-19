a0bc83c208fa412245c5b0c169ce04067a32eccb
sindhu kumari <33718216+sindhusweety@users.noreply.github.com>
2019-10-19 12:26:24 -0400

    Add files via upload

---
+++ generate_sequence.py
@@ -0,0 +1,27 @@
+from keras.engine.saving import load_model
+from keras.preprocessing.sequence import pad_sequences
+# generate a sequence of characters with a language model
+mapping = {' ': 0, 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
+def generate_seq(model, mapping, seq_length, seed_text, n_chars):
+	in_text = seed_text
+	# generate a fixed number of characters
+	for _ in range(n_chars):
+		# encode the characters as integers
+		encoded = [mapping[char] for char in in_text]
+		# truncate sequences to a fixed length
+		encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+		# predict character
+		yhat = model.predict_classes(encoded, verbose=0)
+		# reverse map integer to character
+		out_char = ''
+		for char, index in mapping.items():
+			if index == yhat:
+				out_char = char
+				break
+		# append to input
+		in_text += char
+	return in_text
+inp = 'he is of '
+print(len(inp))
+model = load_model('my_model.h5')
+print(generate_seq(model,mapping,30,inp.lower(),15))
